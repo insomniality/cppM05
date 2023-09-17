6058d8e3cac212bba2ce71a98a13be754da3fbaa
Mikayel Arzumanyan <mikarzum@c2r11s1.42yerevan.am>
2023-09-17 19:01:23 +0400

    sad

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -3,18 +3,25 @@
 
 #include <iostream>
 
+///TODO: READ Proffesional C++ from [250 - 282] Chapter 8 (Then chapter 5)
+///TODO: Read chapter 14 for exceptions, and page 69, 129
+
 class Bureaucrat
 {
 	public:
-		std::string& getName()	const;
-		int& getGrade()	const;
+		std::string getName()	const;
+		int getGrade()	const;
 		void gradeIncrement(); // tars en ashxatelu
 		void gradeDecrement();
-
-	Bureaucrat();
-	Bureaucrat(Bureaucrat& const);
-Bureaucrat	operator=(Bureaucrat& const);
-	~Bureaucrat();
+	
+	public:
+			Bureaucrat();
+			Bureaucrat(const std::string&, int);
+			Bureaucrat(const Bureaucrat&);
+Bureaucrat&	operator=(const Bureaucrat&);
+// a = b <=> a.operator=(b)
+			~Bureaucrat();
+	
 	private:
 		const std::string	name;
 		int					grade;
